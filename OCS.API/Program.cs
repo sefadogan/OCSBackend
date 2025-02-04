b903c89c86f8639f5907c1705a21ec6c9365ac20
Sefa DOĞAN <sefadogan7@gmail.com>
2025-02-04 14:20:18 +0300

    Implement Autofac DI module for services and repos

--- OCS.API/Program.cs
+++ OCS.API/Program.cs
@@ -1,10 +1,16 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using OCS.Business.DependencyResolvers;
 using OCS.DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<OCSDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
 
 
 builder.Services.AddControllers();
---
+++ OCS.Business/DependencyResolvers/AutofacBusinessModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using OCS.DataAccess.Concrete.EntityFramework;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace OCS.Business.DependencyResolvers
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                   .Where(t => t.Name.EndsWith("Service"))
+                   .AsImplementedInterfaces()
+                   .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(typeof(OCSDbContext).Assembly)
+                    .Where(t => t.Name.EndsWith("Repository") || t.Name.EndsWith("Interceptor"))
+                    .AsImplementedInterfaces()
+                    .InstancePerLifetimeScope();
+
+            base.Load(builder);
+        }
+    }
+}
