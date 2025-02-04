cb7e99b0ea1b219f65071a62aa0382621754ce3b
Sefa DOĞAN <sefadogan7@gmail.com>
2025-02-04 14:12:41 +0300

    Fix OrderDTO

--- OCS.Common/DTOs/Orders/OrderDto.cs
+++ OCS.Common/DTOs/Orders/OrderDto.cs
@@ -4,13 +4,15 @@ namespace OCS.Common.DTOs.Orders
 {
     public class OrderDto
     {
+        public long Id { get; set; }
         public int OrderNo { get; set; }
         public string OrderTrackingNo { get; set; }
         public string ShipmentTrackingNo { get; set; }
-        public string CustomerName { get; set; }
-        public string DistrictName { get; set; }
         public OrderStatus Status { get; set; }
         public bool ReleasedForDistribution { get; set; }
         public DateTime CreatedDate { get; set; }
+
+        public CustomerDto Customer { get; set; }
+        public DistrictDto District { get; set; }
     }
 }
